e2bfb7765f3329879687d9e6a06fd190353774b3
eekrain <ardianoption10@gmail.com>
2022-04-21 15:54:11 +0700

    progres 1

--- client/App.tsx
+++ client/App.tsx
@@ -8,108 +8,48 @@
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import React, {useEffect, useMemo, useState} from 'react';
+import {SafeAreaView} from 'react-native';
+import {Text} from 'native-base';
+import MyNativeBaseProvider from './src/components/MyNativeBaseProvider';
+import {NhostReactProvider, useAuthenticationStatus} from '@nhost/react';
+import {NhostApolloProvider} from '@nhost/react-apollo';
+import {nhost} from './src/shared/utils';
+import SplashScreen from './src/components/Overlay/Splashscreen';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const authStatus = useAuthenticationStatus();
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [loadingSplashScreen, setLoadingSplashScreen] = useState(true);
+  const loading = useMemo(
+    () => authStatus.isLoading || loadingSplashScreen,
+    [authStatus, loadingSplashScreen],
+  );
+
+  useEffect(() => {
+    const splash = setTimeout(() => {
+      setLoadingSplashScreen(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(splash);
+    };
+  });
+  if (loading) {
+    return <SplashScreen />;
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <MyNativeBaseProvider>
+      <NhostReactProvider nhost={nhost}>
+        <NhostApolloProvider nhost={nhost}>
+          <SafeAreaView>
+            <Text>ANjing</Text>
+          </SafeAreaView>
+        </NhostApolloProvider>
+      </NhostReactProvider>
+    </MyNativeBaseProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
