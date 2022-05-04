35469b6d34410c3df40c9325fb944fa57d758ca4
eekrain <ardianoption10@gmail.com>
2022-05-05 01:53:56 +0700

    belum ke add semua sebelumnya

---
+++ nhost/migrations/default/1651347464941_set_fk_public_users_metadata_store_id/up.sql
@@ -0,0 +1,5 @@
+alter table "public"."users_metadata" drop constraint "users_metadata_store_id_fkey",
+  add constraint "users_metadata_store_id_fkey"
+  foreign key ("store_id")
+  references "public"."store"
+  ("id") on update cascade on delete set null;
