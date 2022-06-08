236e4366dba7bf26f9b6b5daee844d44b04f0d8b
eekrain <ardianoption10@gmail.com>
2022-06-08 07:58:32 +0700

    squashing transaction function migrations

--- nhost/migrations/default/1654549202219_testing_transaction_items_real_capital_price/up.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE FUNCTION transaction_items_real_capital_price(transaction_item_row transaction_items)
-RETURNS INT AS $$
-  SELECT transaction_item_row.capital_price
-$$ LANGUAGE sql STABLE;
