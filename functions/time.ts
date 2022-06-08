3ff1b1798e8bd74b14886424c3e96e2a415d3e07
eekrain <ardianoption10@gmail.com>
2022-06-08 07:38:20 +0700

    moving deps on /function to root folder

--- functions/time.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export default (req: Request, res: Response) => {
-  return res
-    .status(200)
-    .send(`Hello ${req.query.name}! It's now: ${new Date().toUTCString()}`);
-};
