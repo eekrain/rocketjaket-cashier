016e464b143fea4de6e321dd1da68054a540c9db
eekrain <ardianoption10@gmail.com>
2022-06-08 08:33:10 +0700

    fixing the order of creating the functions

--- nhost/migrations/default/1654649525425_create_transaction_function_computed_fields/down.sql
+++ nhost/migrations/default/1654649525425_create_transaction_function_computed_fields/down.sql
@@ -1,7 +1,7 @@
-DROP FUNCTION IF EXISTS public.transaction_total_purchased_product(transaction)
-DROP FUNCTION IF EXISTS public.transaction_total_profit(transaction)
-DROP FUNCTION IF EXISTS public.transaction_total_transaction(transaction)
-DROP FUNCTION IF EXISTS public.transaction_cash_change(transaction)
-DROP FUNCTION IF EXISTS public.transaction_items_profit(transaction_items)
-DROP FUNCTION IF EXISTS public.transaction_items_subtotal(transaction_items)
-DROP FUNCTION IF EXISTS public.transaction_items_real_capital_price(transaction_items)
+DROP FUNCTION IF EXISTS public.transaction_total_purchased_product(transaction);
+DROP FUNCTION IF EXISTS public.transaction_total_profit(transaction);
+DROP FUNCTION IF EXISTS public.transaction_total_transaction(transaction);
+DROP FUNCTION IF EXISTS public.transaction_cash_change(transaction);
+DROP FUNCTION IF EXISTS public.transaction_items_profit(transaction_items);
+DROP FUNCTION IF EXISTS public.transaction_items_subtotal(transaction_items);
+DROP FUNCTION IF EXISTS public.transaction_items_real_capital_price(transaction_items);
--- nhost/migrations/default/1654649525425_create_transaction_function_computed_fields/up.sql
+++ nhost/migrations/default/1654649525425_create_transaction_function_computed_fields/up.sql
@@ -1,3 +1,12 @@
+CREATE OR REPLACE FUNCTION public.transaction_items_subtotal(transaction_item_row transaction_items)
+ RETURNS integer
+ LANGUAGE sql
+ STABLE
+AS $function$
+  SELECT (transaction_item_row.selling_price - transaction_item_row.selling_price * transaction_item_row.discount / 100) * transaction_item_row.purchase_qty AS subtotal
+$function$;
+
+
 CREATE OR REPLACE FUNCTION public.transaction_total_purchased_product(transaction_row transaction)
  RETURNS bigint
  LANGUAGE sql
@@ -7,6 +16,35 @@ AS $function$
     FROM transaction AS T
     INNER JOIN transaction_items AS TI ON T.invoice_number = TI.transaction_invoice_number
     WHERE T.invoice_number = transaction_row.invoice_number AND TI.transaction_status = 'success'
+$function$;
+
+
+
+CREATE OR REPLACE FUNCTION public.transaction_items_real_capital_price(transaction_item_row transaction_items)
+ RETURNS integer
+ LANGUAGE sql
+ STABLE
+AS $function$
+  SELECT CASE 
+    WHEN TI.capital_price > 0
+      THEN TI.capital_price
+    WHEN IP.override_capital_price > 0
+      THEN IP.override_capital_price
+    ELSE P.capital_price
+    END AS capital_price
+  FROM transaction_items AS TI
+  INNER JOIN inventory_products AS IP ON TI.inventory_product_id = IP.id
+  INNER JOIN products AS P ON IP.product_id = P.id
+  WHERE TI.id=transaction_item_row.id
+$function$;
+
+
+CREATE OR REPLACE FUNCTION public.transaction_items_profit(transaction_item_row transaction_items)
+ RETURNS integer
+ LANGUAGE sql
+ STABLE
+AS $function$
+  SELECT public.transaction_items_subtotal(transaction_item_row) - (public.transaction_items_real_capital_price(transaction_item_row) * transaction_item_row.purchase_qty) AS profit
 $function$;
 
 
@@ -43,44 +81,4 @@ CREATE OR REPLACE FUNCTION public.transaction_cash_change(transaction_row transa
  STABLE
 AS $function$
     SELECT transaction_row.cash_in_amount - public.transaction_total_transaction(transaction_row)
-$function$;
-
-
-
-CREATE OR REPLACE FUNCTION public.transaction_items_profit(transaction_item_row transaction_items)
- RETURNS integer
- LANGUAGE sql
- STABLE
-AS $function$
-  SELECT public.transaction_items_subtotal(transaction_item_row) - (public.transaction_items_real_capital_price(transaction_item_row) * transaction_item_row.purchase_qty) AS profit
-$function$;
-
-
-
-CREATE OR REPLACE FUNCTION public.transaction_items_subtotal(transaction_item_row transaction_items)
- RETURNS integer
- LANGUAGE sql
- STABLE
-AS $function$
-  SELECT (transaction_item_row.selling_price - transaction_item_row.selling_price * transaction_item_row.discount / 100) * transaction_item_row.purchase_qty AS subtotal
-$function$;
-
-
-
-CREATE OR REPLACE FUNCTION public.transaction_items_real_capital_price(transaction_item_row transaction_items)
- RETURNS integer
- LANGUAGE sql
- STABLE
-AS $function$
-  SELECT CASE 
-    WHEN TI.capital_price > 0
-      THEN TI.capital_price
-    WHEN IP.override_capital_price > 0
-      THEN IP.override_capital_price
-    ELSE P.capital_price
-    END AS capital_price
-  FROM transaction_items AS TI
-  INNER JOIN inventory_products AS IP ON TI.inventory_product_id = IP.id
-  INNER JOIN products AS P ON IP.product_id = P.id
-  WHERE TI.id=transaction_item_row.id
 $function$;
