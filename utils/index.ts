b38df33e57d720bb480f22a16964655690c24c2b
eekrain <ardianoption10@gmail.com>
2022-07-10 09:23:19 +0700

    fix getting env vars

--- utils/index.ts
+++ utils/index.ts
@@ -3,3 +3,10 @@ export * from "./resTemplates";
 export * from "./getRandomValue";
 export * from "./firebaseAdmin";
 export * from "./getPeriodicTime";
+
+export const getWhatsappConfig = () => {
+  return {
+    WHATSAPP_API_URL: process.env.WHATSAPP_API_URL || "",
+    WHATSAPP_API_SECRET: process.env.WHATSAPP_API_SECRET || "",
+  };
+};
