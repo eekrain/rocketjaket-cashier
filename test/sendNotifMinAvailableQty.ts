5fe8f17c3aaeedc88596b60d8e434d71de50c887
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-23 02:59:54 +0700

    fix firebaseadmin init

--- test/sendNotifMinAvailableQty.ts
+++ test/sendNotifMinAvailableQty.ts
@@ -6,7 +6,7 @@ export default async (req: Request, res: Response) => {
   const tokens = [
     "f597VEMsRx2CWUzBnVwMo0:APA91bFB3s3O0v4MD8V1429y1bl_l8Ciq2sMsVqjPZXgbE2pvbubJ06djNymGLAOhgqk1ufxFnYe-Y1b_WDJpE2ilfeI1nSlaMgSHlD8Vuc8c3woyQB30zW5egx54WqG3MS_wc1ASJja",
   ];
-  const firebaseAdmin = myFirebaseAdminApp();
+  const firebaseAdmin = await myFirebaseAdminApp();
   const notification = {
     title: "Stok Produk Menipis",
     body: `Produk Celana / Chinos / M hanya tersisa 19!`,
