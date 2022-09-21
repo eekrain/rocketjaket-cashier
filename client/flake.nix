d264b7eb3a74a07574788ab0e6262422f34b26e2
Ardian Eka Candra <ardianoption@gmail.com>
2022-09-21 19:11:33 +0700

    reinit wo androidstudio

--- client/flake.nix
+++ client/flake.nix
@@ -1,33 +1,54 @@
 {
-  description = "My Android app";
+  description = "My Android project";
 
   inputs = {
-    android-nixpkgs = {
-      url = "github:tadfisher/android-nixpkgs";
-
-      # The main branch follows the "canary" channel of the Android SDK
-      # repository. Use another android-nixpkgs branch to explicitly
-      # track an SDK release channel.
-      #
-      # url = "github:tadfisher/android-nixpkgs/stable";
-      # url = "github:tadfisher/android-nixpkgs/beta";
-      # url = "github:tadfisher/android-nixpkgs/preview";
-      # url = "github:tadfisher/android-nixpkgs/canary";
-
-      # If you have nixpkgs as an input, this will replace the "nixpkgs" input
-      # for the "android" flake.
-      #
-      # inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+    android.url = "github:tadfisher/android-nixpkgs";
   };
 
-  outputs = { self, android-nixpkgs }: {
-    packages.x86_64-linux.android-sdk = android-nixpkgs.sdk (sdkPkgs: with sdkPkgs; [
-      build-tools-31-0-0
-      cmdline-tools-latest
-      emulator
-      platform-tools
-      platforms-android-31
-    ]);
-  };
+  outputs = { self, nixpkgs, devshell, flake-utils, android }:
+    {
+      overlay = final: prev: {
+        inherit (self.packages.${final.system}) android-sdk android-studio;
+      };
+    }
+    //
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [
+            devshell.overlay
+            self.overlay
+          ];
+        };
+      in
+      {
+        packages = {
+          android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+            # Useful packages for building and testing.
+            build-tools-30-0-2
+            cmdline-tools-latest
+            emulator
+            platform-tools
+            platforms-android-30
+
+            # Other useful packages for a development environment.
+            # sources-android-30
+            # system-images-android-30-google-apis-x86
+            # system-images-android-30-google-apis-playstore-x86
+          ]);
+
+          # android-studio = pkgs.androidStudioPackages.stable;
+          # android-studio = pkgs.androidStudioPackages.beta;
+          # android-studio = pkgs.androidStudioPackages.preview;
+          # android-studio = pkgs.androidStudioPackage.canary;
+        };
+
+        devShell = import ./devshell.nix { inherit pkgs; };
+      }
+    );
 }
