572ae602704a1fca4a417a0c2b26bc22fa0bf64e
Ardian Eka Candra <ardianoption@gmail.com>
2022-10-17 04:43:44 +0700

    using nix-rn template

--- client/flake.nix
+++ client/flake.nix
@@ -11,7 +11,9 @@
   outputs = { self, nixpkgs, devshell, flake-utils, android }:
     {
       overlay = final: prev: {
-        inherit (self.packages.${final.system}) android-sdk android-studio;
+        # If u need android studio set up, u should add android-studio to overlay below like:
+        # android-studio
+        inherit (self.packages.${final.system}) android-sdk;
       };
     }
     //
@@ -25,30 +27,40 @@
             self.overlay
           ];
         };
-      in
-      {
-        packages = {
-          android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+
+        androidConfig = {
+          defaultBuildToolsVersion = "31.0.0";
+          sdkPkgs = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
             # Useful packages for building and testing.
+            # make sure to add defaultBuildToolsVersion
+            build-tools-31-0-0
+            # If when building the apps needs to add other version of build tools, u can add it too
+            build-tools-30-0-3
             cmdline-tools-latest
             emulator
             platform-tools
-            build-tools-30-0-2
-            build-tools-30-0-3
-            build-tools-31-0-0
-            platforms-android-30
             platforms-android-31
+            patcher-v4
 
             # Other useful packages for a development environment.
-            sources-android-30
-            system-images-android-30-google-apis-x86
+            # sources-android-30
+            # system-images-android-30-google-apis-x86
             # system-images-android-30-google-apis-playstore-x86
           ]);
-
-          android-studio = pkgs.androidStudioPackages.stable;
         };
+      in
+      {
+        packages = {
+          android-sdk = androidConfig.sdkPkgs;
 
-        devShell = import ./devshell.nix { inherit pkgs; };
+          # If u need android-studio, u can choose one of these channels
+          # Be sure to add it to overlay above
+          # android-studio = pkgs.androidStudioPackages.stable;
+          # android-studio = pkgs.androidStudioPackages.beta;
+          # android-studio = pkgs.androidStudioPackages.preview;
+          # android-studio = pkgs.androidStudioPackage.canary;
+        };
+        devShell = import ./devshell.nix { inherit pkgs androidConfig; };
       }
     );
 }
